e916a0871a12ae338cbff676a599c58a71629c22
xvii-tommy <thomas.easterbrook97@gmail.com>
2021-02-13 19:42:18 +0000

    Cipher using ASCII code

--- alternative_caesar_cipher.py
+++ alternative_caesar_cipher.py
@@ -1,5 +1,10 @@
-x = ord("a")
-print(x, chr(x))
-
-x = x + 1
-print(x, chr(x))
+x = input("enter your message here")
+print("message 1")
+for i in range(26):
+    for j in x:
+         k = (ord(j) + i)
+         if k > 122:
+             k = k - 26
+         print(chr(k))
+    print("message", i + 2)
+    #zkc
